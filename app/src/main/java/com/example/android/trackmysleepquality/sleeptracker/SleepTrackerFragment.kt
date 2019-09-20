49f4cedcebcd6ca869adafff58890fe5b35d64b8
henryfebryan <henry_febryan@yahoo.com>
2019-09-20 13:24:28 +0700

    Add RecyclerView,Adapter,ViewHolder

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -59,6 +59,15 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         binding.setLifecycleOwner(this) //necessary, so databinding can observe livedata updates
 
         sleepTrackerViewModel.navigateToSleepQuality.observe(this, Observer { night ->
